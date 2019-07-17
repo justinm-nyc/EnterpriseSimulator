c60d36424ddbc05998c8cc7eeec9ff1e45321ef3
justinm-nyc <justin.martinelli@oracle.com>
2019-07-16 21:36:58 -0400

    Added more styling

--- public/app.js
+++ public/app.js
@@ -1,9 +1,14 @@
-
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!',
-      artilleryResults: ''
+      artilleryResults: '',
+      chosenWorkload: '',
+      chosenWorkloadProfile: '',
+      workloadSelected: false,
+      isHealthCare: false,
+      isSurvey: false,
+      isRamp: false,
+      isOscilating: false
     },
     methods: {
       fetchResults: function() {
@@ -15,6 +20,32 @@ var app = new Vue({
       }).catch(function(error){
           console.error("fetchResults failed", error.toString());
       });
+      },
+      setWorkload: function(workload){
+        this.chosenWorkload = workload
+        this.workloadSelected = true
+        console.log(this.chosenWorkload + " chosen")
+
+        if(this.chosenWorkload == 'healthCare'){
+          this.isHealthCare = true
+          this.isSurvey = false
+        }else if(this.chosenWorkload == 'survey'){
+          this.isHealthCare = false
+          this.isSurvey = true          
+        }
+      },
+      setWorkloadProfile: function(workloadProfile){
+        this.chosenWorkloadProfile = workloadProfile
+        console.log(this.chosenWorkloadProfile + " chosen")
+
+        if(this.chosenWorkloadProfile == 'ramp'){
+          this.isRamp = true
+          this.isOscilating = false
+        }else if(this.chosenWorkloadProfile == 'oscilating'){
+          this.isRamp = false
+          this.isOscilating = true          
+        }
       }
-    }
+    },
+
 })
